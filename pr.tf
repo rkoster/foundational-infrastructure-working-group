06b6003b49c38908293b9c27c70a19bbf563dc61
rkoster <hi@rkoster.dev>
2021-10-13 17:19:25 +0200

    Refactor to project per sub group

--- pr.tf
+++ pr.tf
@@ -1,51 +1,41 @@
 data "github_repository_pull_requests" "repos" {
-
-  for_each = var.repositories
-
+  for_each        = local.repositories
   base_repository = split("/", each.key)[1]
   owner           = split("/", each.key)[0]
   state           = "open"
-
 }
+
 locals {
-  repos_active_prs = compact([
-    for pr in data.github_repository_pull_requests.repos : length(pr.results) == 0 ? "" : "${pr.owner}/${pr.base_repository}"
-  ])
+  repositories = merge([for project in var.projects : project.repositories]...)
+
   pr_data = merge(flatten([
-    for repo in local.repos_active_prs : [
-      for pr in data.github_repository_pull_requests.repos[repo].results :
-      {
-        "${repo}/${pr.number}" : {
-          number = pr.number,
-          link   = "https://github.com/${repo}/pull/${pr.number}"
-          label  = lookup(var.repositories, repo).label
+    for project in var.projects : [
+      for repo, config in project.repositories : [
+        for pr in data.github_repository_pull_requests.repos[repo].results :
+        {
+          "${repo}/${pr.number}" : {
+            column_id = project.inbox_column_id
+            number    = pr.number,
+            link      = "https://github.com/${repo}/pull/${pr.number}"
+          }
         }
-      }
+      ]
     ]
   ])...)
-  columns = {
-    "INBOX" : {}
-    "PENDING_REVIEW" : {},
-    "CHANGES_REQUESTED" : {},
-  }
 }
 resource "github_project_card" "card" {
   lifecycle {
     ignore_changes = [column_id]
   }
   for_each  = local.pr_data
-  column_id = "16321587"
-  note      = <<EOF
-__${each.value.label}__
-
-${each.value.link}
-EOF
-
-}
-output "active_prs" {
-  value = local.repos_active_prs
+  column_id = each.value.column_id
+  note      = each.value.link
 }
 
 output "pr_data" {
   value = local.pr_data
 }
+
+# output "repositories" {
+#   value = local.repositories
+# }
